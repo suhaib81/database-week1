dc1c4a72e66a6ad01a2f20da014adc905a10eca3
Suhaib <suhaib81@gmail.com>
2017-04-22 22:40:36 +0200

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -11,17 +11,22 @@ var config = JSON.parse(fs.readFileSync("./config-secret.json"))
 
 router.get('/', function (req, res) {
 
+
+
+
     var pool = mysql.createPool(config);
+    var todoName = pool.escape('workout')
     var query1 = "SELECT count(1) as total FROM todos";
     var query2 = "select * from todos where done = 1";
     var query3 = "select * from todos where done <> 1";
     var query4 = "select * from todos order by Due desc";
     var query5 = "select * from todos  order by Due desc LIMIT 1";
     var query6 = "select * from todos where Name like '%databases%'";
+    var query7 = `insert into todos (Name) values('${todoName}')`;
 
     var return_data = {};
 
-    async.parallel([
+    async.parallel([ 
 
        function(parallel_done) {
            pool.query(query1, {}, function(err, results) {
@@ -81,3 +86,33 @@ router.get('/', function (req, res) {
          
     });
 });
+
+
+router.get('/delete/:id', function (req, res) {
+
+var connection = mysql.createConnection(config);
+connection.connect();
+var id = connection.escape(req.params.id);
+connection.query('SELECT FROM todos where id='+id, function (error, results, fields) {
+    console.log(results);
+});
+connection.end();
+res.send('deleted'+id)
+
+});
+
+router.get('/update/:id', function (req, res) {
+var connection = mysql.createConnection(config);
+connection.connect();
+var id = connection.escape(req.params.id);
+var name = connection.escape(req.params.name);
+connection.query('update todos set Name = '+name+' where id='+id, function (error, results, fields) {
+    console.log(results);
+});
+connection.end();
+res.send('deleted'+id)
+
+});
+
+
+       
